69e996d4360edf135a29025798ffda30702e376c
carolisteclopes <151862073+carolisteclopes@users.noreply.github.com>
2025-06-09 13:09:45 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Script para toggle do menu mobile
-const menuBtn = document.getElementById('menu-btn');
-const menu = document.getElementById('menu');
-
-menuBtn.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
-});
