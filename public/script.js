3816a6575eecf568834d729b49414d368ecac9f5
carolisteclopes <151862073+carolisteclopes@users.noreply.github.com>
2025-06-09 13:31:44 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,9 +1,3 @@
-function enviarMensagem(e) {
-  e.preventDefault();
-  const feedback = document.getElementById("feedback");
-  feedback.textContent = "Mensagem enviada com sucesso!";
-  feedback.style.color = "#d63384";
-  setTimeout(() => {
-    feedback.textContent = "";
-  }, 3000);
+function enviarMensagem() {
+  alert('Mensagem enviada com sucesso!');
 }
