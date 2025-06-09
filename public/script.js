0ff3ed8ad4cf9a70f0c3f66dcc5b5a1ef5f21515
carolisteclopes <151862073+carolisteclopes@users.noreply.github.com>
2025-06-09 13:51:37 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -14,5 +14,5 @@ const menuBtn = document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
 
 menuBtn.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
+  menu.classList.toggle('show');
 });
